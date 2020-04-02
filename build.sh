9e19fdaec5d944f953e17b3cc37539e939ee9be2
yuzhipeng <zpyu@alauda.io>
2020-04-02 22:35:41 +0800

    dds

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker build -t buildjar:build . -f Dockerfile.build
 
 docker container create --name extract buildjar:build
-docker container cp extract:/src/build/libs/nexus-proxy-2.3.0.jar /nexus/nexus-proxy/
+docker container cp extract:/src/build/libs/nexus-proxy-2.3.0.jar /home/jenkins/agent/workspace/devops/devops-nexus-proxy
 docker container rm -f extract

5448c192c2974b1c9d2d24de652404424ada8b6f
yuzhipeng <zpyu@alauda.io>
2020-04-02 21:59:32 +0800

    dd

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker build -t buildjar:build . -f Dockerfile.build
 
 docker container create --name extract buildjar:build
-docker container cp extract:/src/build/libs/useless-nexus-proxy-2.3.0.jar /nexus/nexus-proxy/src/build/libs/useless-nexus-proxy-2.3.0.jar
+docker container cp extract:/src/build/libs/nexus-proxy-2.3.0.jar /nexus/nexus-proxy/nexus-proxy-2.3.0.jar
 docker container rm -f extract
